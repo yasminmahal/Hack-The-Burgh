29e35dd5c06ddfe8875e191812535944f1d52825
yasminmahal <s1523085@sms.ed.ac.uk>
2018-03-10 21:39:22 +0000

    hello

--- SavetheWorld/app/src/main/java/com/example/yasmin/savetheworld/EmergencyMap.kt
+++ SavetheWorld/app/src/main/java/com/example/yasmin/savetheworld/EmergencyMap.kt
@@ -1,18 +1,28 @@
 package com.example.yasmin.savetheworld
 
+import android.content.pm.PackageManager
 import android.location.Location
+import android.location.LocationListener
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v4.content.ContextCompat.checkSelfPermission
+import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 
-class EmergencyMap : AppCompatActivity(), OnMapReadyCallback {
+
+class EmergencyMap : AppCompatActivity(), OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private lateinit var mMap: GoogleMap
     private lateinit var mGoogleApiClient: GoogleApiClient
@@ -20,6 +30,7 @@ class EmergencyMap : AppCompatActivity(), OnMapReadyCallback {
     var mLocationPermissionGranted = false
     private var mLastLocation: Location? = null
     private var mCurrentLocation: Location? = null
+    private lateinit var locationCallback: LocationCallback
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,21 +39,110 @@ class EmergencyMap : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                for (location in locationResult.locations) {
+                    mCurrentLocation = location
+                }
+            }
+        }
+
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build()
     }
 
+
+    override fun onStart() {
+        super.onStart()
+        mGoogleApiClient.connect()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (mGoogleApiClient.isConnected) {
+            mGoogleApiClient.disconnect()
+        }
+    }
+
+    fun createLocationRequest() {
+        val mLocationRequest = LocationRequest()
+        mLocationRequest.interval = 5000
+        mLocationRequest.fastestInterval = 1000
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+
+        val permissionCheck = checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, locationCallback, null)
+
+        }
+    }
+
+    override fun onConnected(connectionHint: Bundle?) {
+        try {
+            createLocationRequest()
+        } catch (ise: IllegalStateException) {
+            println("IllegalStateException thrown [onConnected]")
+        }
+        if (checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient)
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    override fun onLocationChanged(current: Location?) {
+        if (current == null) {
+            println("[OnLocationChanged] Location unknown")
+        } else {
+
+            mCurrentLocation = current
+
+            println(""" [onLocationChanged] Lat/long now
+                (${current.getLatitude()},
+                ${current.getLongitude()})"""
+            )
+        }
+    }
+
+    override fun onConnectionSuspended(flag: Int) {
+        println(">>>> onConnectionSuspended")
+    }
+
+    override fun onConnectionFailed(result: ConnectionResult) {
+        println(">>>> onConnectionFailed")
+    }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        try
-        {
-                mMap.isMyLocationEnabled = true
-            }
-            catch (se : SecurityException)
-            {
-                println("Security exception thrown [onMapReady]")
-            }
+        try {
+            mMap.isMyLocationEnabled = true
+        } catch (se: SecurityException) {
+            println("Security Exception thrown [onMapReady]")
+        }
 
-            mMap.uiSettings.isMyLocationButtonEnabled = true
+        mMap.uiSettings.isMyLocationButtonEnabled = true
+        val start = LatLng(55.9445, -3.18736)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(start, 15.toFloat()))
+        mMap.uiSettings.isZoomControlsEnabled = true
+        mMap.uiSettings.isZoomGesturesEnabled = true
+    }
+
+    override fun onProviderDisabled(p0: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderEnabled(p0: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
